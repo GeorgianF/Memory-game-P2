0faf0dbf16f949a5584139d7d21810d218528a9e
GeorgianF <georgianfurtuna@yahoo.com>
2021-12-31 10:10:46 +0000

    add data-name to divs in html for JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 const cards = document.querySelectorAll('.basic-card');
 
+let cardFlipped = false;
+let frontFace, backFace;
+
 function rotateCard () {
-    this.classList.toggle('rotate')
+    this.classList.add('rotate');
+
+    if (!cardFlipped) {
+        cardFlipped = true;
+        frontFace = this;
+    } else {
+        cardFlipped = false;
+        backFace = this;
+    }
+
+
 }
 
 cards.forEach(card => card.addEventListener('click', rotateCard))
